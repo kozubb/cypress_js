6a0cb925ad2e51b57f79dd5715a44c4301895fa5
kozubb <piotrek.kozubb@gmail.com>
2025-12-04 21:40:30 +0100

    remove junit

--- cypress.config.js
+++ cypress.config.js
@@ -3,34 +3,15 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/tests/**/*.cy.js",
-    experimentalMemoryManagement: true,
-    trashAssetsBeforeRuns: false,
-    retries: {
-      runMode: 1,
-      openMode: 0,
-    },
-    blockHosts: ["*.google-analytics.com", "*.contentsquare.net"],
-    redirectionLimit: 100,
-    waitForAnimations: true,
-    chromeWebSecurity: false,
-    defaultCommandTimeout: 18000,
-    pageLoadTimeout: 30000,
-    requestTimeout: 25000,
-    responseTimeout: 60000,
-    screenshotOnRunFailure: true,
-    video: false,
     reporter: "cypress-multi-reporters",
     reporterOptions: {
-      reporterEnabled: "mochawesome,cypress-junit-reporter",
+      reporterEnabled: "mochawesome",
       mochawesomeReporterOptions: {
         reportDir: "cypress/reports/mocha",
         quiet: true,
         overwrite: false,
         html: false,
         json: true,
-      },
-      cypressJunitReporterOptions: {
-        mochaFile: "cypress/reports/test-results-[hash].xml",
       },
     },
   },
