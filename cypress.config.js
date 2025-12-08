2cc60f1319657e10142b4b7ab8cd4226f0b478ca
kozubb <piotrek.kozubb@gmail.com>
2025-12-08 17:31:21 +0100

    revert changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,25 @@
-const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/tests/**/*.cy.js",
+    experimentalMemoryManagement: true,
+    trashAssetsBeforeRuns: false,
+    retries: {
+      runMode: 1,
+      openMode: 0,
+    },
+    blockHosts: ["*.google-analytics.com", "*.contentsquare.net"],
+    redirectionLimit: 100,
+    waitForAnimations: true,
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 18000,
+    pageLoadTimeout: 30000,
+    requestTimeout: 25000,
+    responseTimeout: 60000,
+    screenshotOnRunFailure: true,
+    video: false,
     reporter: "cypress-multi-reporters",
     reporterOptions: {
-      reporterEnabled: "mochawesome,ctrf-mocha-reporter",
+      reporterEnabled: "mochawesome,cypress-junit-reporter",
       mochawesomeReporterOptions: {
         reportDir: "cypress/reports/mocha",
         quiet: true,
@@ -13,8 +27,8 @@ module.exports = defineConfig({
         html: false,
         json: true,
       },
-      ctrfMochaReporterOptions: {
-        outputDir: "cypress/reports/ctf",
+      cypressJunitReporterOptions: {
+        mochaFile: "cypress/reports/test-results-[hash].xml",
       },
     },
   },
