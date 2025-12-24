95380262d85bcf25302bd63587fddfe0901e76d7
kozubb <piotrek.kozubb@gmail.com>
2025-12-24 11:38:31 +0100

    add methods for PLP

--- cypress/pages/SauceDemo/ProductListing.js
+++ cypress/pages/SauceDemo/ProductListing.js
@@ -1,19 +1,73 @@
-class ProductListing {
-  shoppingCart = '[data-test="shopping-cart-link"]';
+export default class ProductListing {
+  // #region Helpers on UI elements (dynamic elements)
+
+  // Return selector for add to order button
+  addToOrderButton(productName) {
+    return cy.get(`[data-test="add-to-cart-${productName}"]`);
+  }
+
+  // Return selector for remove product button
+  removeProductButton(productName) {
+    return cy.get(`[data-test="remove-${productName}"]`);
+  }
+
+  // Return selector for product image
+  productImage(productName) {
+    return cy.get(`[data-test="inventory-item-${productName}-img"]`);
+  }
+
+  // #endregion
 
   // #region Actions on UI elements
+
+  // Press add to order button for specific product
+  pressAddToOrderButton(productName) {
+    this.addToOrderButton(productName).click();
+    return this;
+  }
+
+  // Press remove product button for specific product
+  pressRemoveProductButton(productName) {
+    this.removeProductButton(productName).click();
+    return this;
+  }
+
+  // Press product image for specific product for opening pdp
+  pressProductImage(productName) {
+    this.productImage(productName).click();
+    return this;
+  }
+
+  // Press shopping cart icon
+  pressShoppingCartIcon() {
+    cy.get('[data-test="shopping-cart-link"]').click();
+    return this;
+  }
 
   // #endregion
 
   // #region Validations on UI elements
 
-  // Check if the shopping cart is visible on the page
+  // Check if shopping cart icon is visible
   checkIfCartIsVisible() {
-    cy.log("Validating if cart icon is visible");
-    cy.get(this.shoppingCart).should("be.visible");
+    cy.get('[data-test="shopping-cart-link"]').should("be.visible");
+    return this;
+  }
+
+  // Check if remove button is visible for specific product
+  checkIfRemoveButtonIsVisible(productName) {
+    this.removeProductButton(productName).should("be.visible");
+    return this;
+  }
+
+  // Check shopping cart amount
+  checkShoppingCartAmount(expectedNumber) {
+    cy.get('[data-test="shopping-cart-badge"]').should(
+      "have.text",
+      expectedNumber
+    );
+    return this;
   }
 
   // #endregion
 }
-
-export default ProductListing;
